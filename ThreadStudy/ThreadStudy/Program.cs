2713b4b508233ef6fa5e9639d1125cf373ac1cc7
shun-1129 <shun.1129.y@gmail.com>
2023-06-07 00:14:25 +0900

    Add : ThreadStudy processing

--- ThreadStudy/ThreadStudy/Program.cs
+++ ThreadStudy/ThreadStudy/Program.cs
@@ -4,8 +4,36 @@
     {
         public static void Main(string[] args)
         {
-            Worker worker = new Worker();
-            worker.Executor();
+            var tc = new thread_class();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var t = new Thread(new ParameterizedThreadStart(SampleProc));
+                t.Start(tc);
+            }
+        }
+
+        public static void SampleProc(object obj)
+        {
+            var tc = (thread_class)obj;
+            Console.WriteLine(tc.Sum());
+        }
+    }
+
+    public class thread_class
+    {
+        private int count;
+
+        public int Sum()
+        {
+            count = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                count++;
+            }
+
+            return count;
         }
     }
 }
